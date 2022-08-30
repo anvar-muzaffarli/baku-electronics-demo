fcd4aff5fa53213599efda38b4063e7a0e95e283
anvar-muzaffarli <prof.anvar2013@gmail.com>
2022-08-30 17:51:21 +0400

    diplom ishi ucun nezerde tutulan homepage slider hazirlandi

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
